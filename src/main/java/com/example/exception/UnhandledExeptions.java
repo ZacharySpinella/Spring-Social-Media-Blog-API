eaa136bc4828bea9e756c744cec35152ae56e983
Zachary Spinella <zacharyspinella4@gmail.com>
2025-02-20 19:32:28 +0000

    exeption handeling

--- src/main/java/com/example/exception/UnhandledExeptions.java
+++ src/main/java/com/example/exception/UnhandledExeptions.java
@@ -1,5 +1,20 @@
 package com.example.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class UnhandledExeptions {
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public @ResponseBody String handleMissingParams(MissingServletRequestParameterException ex)
+    {
+        return ex.getParameterName();
+    }
     
 }
